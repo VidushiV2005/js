b943d3b203711d68f09660c84538f9fedf3db45d
Vidushi Verma <vidushiverma2005@gmail.com>
2024-11-06 16:35:10 +0000

    variables

---
+++ .devcontainer/basics/variables.js
@@ -0,0 +1,12 @@
+let accountId = 23456
+var accountName = "vidushi"
+const accountEmail ="vidushi@gmail.com"
+accountPassword ="1234verma"
+console.log(accountId);
+console.table([accountEmail,accountId,accountName,accountPassword])
+
+/*
+Prefer not to use var
+because of issue in block scope and functional scope
+*/
+// accountEmail = 2 // not allowed
